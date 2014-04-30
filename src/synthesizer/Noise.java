5cf0753b4aa6fb5c4daea90b374751a62d13b7fb
dabauldterr <terencefarrellis@hotmail.com>
2014-04-30 13:42:31 +0100

    updated to include harmonics
    distorted, clipped, aliased or other wave forma being generated???????????????????

--- src/synthesizer/Noise.java
+++ src/synthesizer/Noise.java
@@ -8,21 +8,43 @@ package synthesizer;
  *
  * @author se413006
  */
-public class Noise extends Oscillators{
+public class Noise extends Oscillators {
+ 
+    
    
-   
-   public Noise(){
-   
+    public Noise(double _amp,double _fhz){
+        fhz=_fhz;
+        
+        amp=_amp;
     }
-   
-   
-    public double[] output() {
-        double[] signal = new double[44100];
+    
+     public double[] output() {
+        double[] wave = new double[Fs];
+        double Amp;
+        double[] harmonicScale = new double[Fs];
 
-        for (int i = 0; i < signal.length; i++) {
-            signal[i] = 1 * (2 * (double) Math.random() - 1);
+        double pi = Math.PI;
+        
+        int K = (int) Math.floor(Fs * 0.5 / fhz);
+
+        System.out.println("K freq is" + K);
+        double[] harmonic = new double[Fs];
+
+        for (int k = 1; k < K; k=k+1) {
+            //compute the amplitude for each sinusoid based on harmonic number k
+            Amp = (double) Math.pow(k, -1); //Amp=(1/(double)k);
+            Amp = 2 * Amp / pi;
+            //create a sinewave of frequency k*fhz
+            harmonic = new Sine(amp,((1/Math.pow(2, k/12) )* fhz)).output();
+            //scale(multiply) the harmonic by the Amplitude
+            harmonicScale = scale(harmonic, Amp);
+            
+            //build the sawtooth by adding this harmonic to wave
+            //at each cycle of the for loop one more harmonic is added to wave
+            wave = addArray(wave, harmonicScale);
         }
 
-        return signal;
+        return wave;
     }
+    
 }
